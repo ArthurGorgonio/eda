8f2446bdcf1f199c56973ba08767d25ac69e0197
ArthurGorgonio <gorgonioarthur@gmail.com>
2019-03-07 18:18:26 -0300

    Testing bubble sort function

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+int main() {
+  int *v, i, n;
+  printf("Quantos elementos o vetor possui? ");
+  scanf("%d",&n);
+  v = (int *) malloc(n * sizeof(int));
+  for (i = 0; i < n; i++) {
+    printf("elemento %d \n", (i + 1));
+    scanf("%d",&v[i]);
+  }
+  bubble_sort(v, n);
+  printf("\nApós bubble sort!\n");
+  for (i = 0; i < n; i++) {
+    printf("%d\t", v[i]);
+  }
+  printf("\n");
+  return 0;
+}
