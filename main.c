8494968d4ed65d031932bb18329b33de80db8d1f
ArthurGorgonio <gorgonioarthur@gmail.com>
2019-05-28 07:10:08 -0300

    creating the main with all sort algorithms

--- main.c
+++ main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "sort.h"
+#include "heap.h"
+#include "insertion.h"
+#include "merge.h"
+#include "mergeHybrid.h"
+#include "quick.h"
+#include "quickHybrid.h"
 
-int main() {
-  int *v, i, n;
-  printf("Quantos elementos o vetor possui? ");
-  scanf("%d",&n);
-  v = (int *) malloc(n * sizeof(int));
-  for (i = 0; i < n; i++) {
-    printf("elemento %d \n", (i + 1));
-    scanf("%d",&v[i]);
+int main(int argc, char *argv[]) {
+  int start, stop, step, nIterations, maxRandomRange, length;
+  start = atoi(argv[1]);
+  stop = atoi(argv[2]);
+  step = atoi(argv[3]);
+  nIterations = atoi(argv[4]);
+  maxRandomRange = atoi(argv[5]);
+  for (length = start; length <= stop; length = length + step) {
+    runHeap(length, nIterations, maxRandomRange);
+    printf("\nHeap %d", length);
+    runInsertion(length, nIterations, maxRandomRange);
+    printf("\nInsertion %d", length);
+    runMerge(length, nIterations, maxRandomRange);
+    printf("\nMerge %d", length);
+    runMergeHybrid(length, nIterations, maxRandomRange);
+    printf("\nMerge Hybrid %d", length);
+    runQuick(length, nIterations, maxRandomRange);
+    printf("\nQuick %d", length);
+    runQuickHybrid(length, nIterations, maxRandomRange);
+    printf("\nQuick Hybrid %d", length);
   }
-  heapsort(v, n);
-  printf("\nApós HEAP sort!\n");
-  for (i = 0; i < n; i++) {
-    printf("%d\t", v[i]);
-  }
-  printf("\n");
   return 0;
 }
