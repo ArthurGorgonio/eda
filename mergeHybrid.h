4c9b4c273bc5d5126f0132d798c22fad1c8801bd
ArthurGorgonio <gorgonioarthur@gmail.com>
2019-05-28 07:09:40 -0300

    Change function name

--- mergeHybrid.h
+++ mergeHybrid.h
@@ -1,3 +1,3 @@
 void hybridMergeSort(float *v, int i, int f, int threshold);
 
-void runHybridMerge(int length, int nIterations, int maxRandomRange);
+void runMergeHybrid(int length, int nIterations, int maxRandomRange);
